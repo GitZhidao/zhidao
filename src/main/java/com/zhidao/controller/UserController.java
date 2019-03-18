33c561ee4f1a36d674f0375679ed03f907a62e77
Lvwenchao <giswenchao@gmail.com>
2019-03-18 22:55:46 +0800

    修改lv

---
+++ src/main/java/com/zhidao/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.zhidao.controller;
+
+import com.zhidao.common.ServerResponse;
+import com.zhidao.pojo.User;
+import com.zhidao.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @author:
+ * @data: 2019/3/18 12:12
+ * @description:
+ * @Version: 1.0
+ **/
+@Controller
+@RequestMapping("/user")
+@SessionAttributes(names = {"user"},types = {User.class})
+public class UserController {
+    @Autowired
+    IUserService iUserService;
+
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public ModelAndView Login(@RequestParam("id") int id, @RequestParam("password") String password){
+        ServerResponse serverResponse=iUserService.findUser(id);
+        if(serverResponse.isSuccess()){
+            ModelAndView mv=new ModelAndView("main");
+            mv.addObject("user",serverResponse.getData());
+            return mv;
+        }
+        ModelAndView mv=new ModelAndView("main");
+        mv.addObject("user",serverResponse.getData());
+        return mv;
+    }
+
+    @RequestMapping(value = "/regist",method = RequestMethod.POST)
+    public ServerResponse regist(@ModelAttribute("user") User user){
+       
+
+    }
+
+}
